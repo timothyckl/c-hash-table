79600e025cc5599a2ba296010dd35a9c90604e0a
timothyckl <timothy.ckl@outlook.com>
2025-10-18 12:18:06 +0800

    Added hash function

--- src/hash_table.c
+++ src/hash_table.c
@@ -1,8 +1,10 @@
-#include <stddef.h>
+#include "hash_table.h"
+#include <assert.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#include "hash_table.h"
 
 static ht_item *ht_new_item(const char *key, const char *value) {
   ht_item *item = malloc(sizeof(ht_item));
@@ -39,4 +41,43 @@ void delete_hash_table(hash_table *table) {
 
   free(table->items);
   free(table);
+}
+
+static bool is_prime(int num) {
+  if (num <= 1) {
+    return false;
+  }
+  if (num == 2) {
+    return true;
+  }
+  if (num % 2 == 0) {
+    return false;
+  }
+
+  for (int i = 3; i * i <= num; i += 2) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+static int ht_hash(const char *string, const int alpha, const int num_buckets) {
+  const int len_str = strlen(string);
+
+  if (!is_prime(alpha) || alpha < len_str) {
+    fprintf(stderr, "Error: alpha must be a prime number greater than the "
+                    "string length.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  long hash = 0;
+
+  for (int i = 0; i < len_str; i++) {
+    hash += (long)pow(alpha, len_str - (i + 1)) * string[i];
+    hash = hash % num_buckets;
+  }
+
+  return (int)hash;
 }
